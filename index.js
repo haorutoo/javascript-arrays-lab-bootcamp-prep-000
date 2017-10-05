74cb00699c573ee7ca8b6641656362701020bbf5
haorutoo <haorutoo@gmail.com>
2017-10-05 04:47:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function destructivelyRemoveLastKitten(name){
   kittens.pop(name)
   return kittens
 }
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return kittens
+}
